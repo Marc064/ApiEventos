e1d771202c722e594b41957728a4a2eff523256f
Marc064 <95501065+Marc064@users.noreply.github.com>
2024-03-17 21:08:56 -0500

    cors

--- src/main/java/com/edu/uptc/apieventos/controllers/AssistantController.java
+++ src/main/java/com/edu/uptc/apieventos/controllers/AssistantController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/assistant")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AssistantController {
     @Autowired
     private AssistantService assistantService;
--- src/main/java/com/edu/uptc/apieventos/controllers/EventController.java
+++ src/main/java/com/edu/uptc/apieventos/controllers/EventController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/event")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EventController {
     @Autowired
     private EventService eventService;
